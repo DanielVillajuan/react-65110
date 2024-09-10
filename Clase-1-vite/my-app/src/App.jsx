ab270693d81549a299a705a268bdcd95d6d18eb7
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-09-09 22:46:25 -0300

    init del proyecto

---
+++ Clase-1-vite/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <span>Holaaa mi primer app con vite</span>
+    </div>
+  );
+}
+
+export default App;
