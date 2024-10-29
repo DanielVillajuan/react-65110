4200721802a92e2527f3c9700aad3aa3792a24a0
Benjamín Peyraga <benjapey99@gmail.com>
2024-10-28 22:15:22 -0300

    Esta pobre pero funcional

--- Clase-8/src/Components/SubirImagen.jsx
+++ Clase-8/src/Components/SubirImagen.jsx
@@ -1,19 +1,25 @@
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import firebaseConfig from "../config/firebase";
+import { useState } from "react";
 
 const SubirImagen = () => {
+  const [url, setUrl] = useState("");
   const handleChange = async (e) => {
     const refStorage = ref(firebaseConfig.storage, "imagen");
     const fileSnap = await uploadBytes(refStorage, e.target.files[0]);
     const fileUrl = await getDownloadURL(fileSnap.ref);
-    console.log(fileUrl); // url del archivo subido a storage de firebase
     // { desc: '', price: '', category: '', disccount: '', fileSrc: '' }
+    setUrl(fileUrl);
   };
 
+  const handleClick = () => {
+    console.log(url);
+  }
   return (
     <div className="flex flex-col gap-y-4">
       <span>Subir imagen</span>
       <input type="file" onChange={handleChange} />
+      <button onClick={handleClick}>Obtener Url por consola</button>
     </div>
   );
 };
